2f257ccfc8c6d0ab7ae46de00a5757f60a56ba86
Daniel Saxil-Nielsen <daniel@saxil-nielsen.com>
2014-03-10 15:52:05 +0000

    Added auto generation of documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,28 @@ undef:true, unused:true, curly:true, browser:true, jquery:true, node:true, inden
 module.exports = function (grunt) {
 
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
 			all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
 		},
 		nodeunit: {
 			all: ['tests/*.js']
+		},
+		yuidoc: {
+			compile: {
+				name: '<%= pkg.name %>',
+				description: '<%= pkg.description %>',
+				version: '<%= pkg.version %>',
+				url: '<%= pkg.homepage %>',
+				options: {
+					paths: 'lib',
+					outdir: 'docs'
+				}
+			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-nodeunit');
-	grunt.registerTask('default', ['jshint', 'nodeunit']);
+	grunt.loadNpmTasks('grunt-contrib-yuidoc');
+	grunt.registerTask('default', ['jshint', 'nodeunit', 'yuidoc']);
 };
