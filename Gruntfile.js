0fc6d01cf21e4f051f70d8b1e4e278f59e2c89fc
Daniel Saxil-Nielsen <daniel@saxil-nielsen.com>
2015-05-13 13:34:18 -0700

    Removed the nodeunit test

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,6 @@ module.exports = function (grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	/*grunt.loadNpmTasks('grunt-contrib-nodeunit');*/
 	grunt.registerTask('default', ['jshint' /*, 'nodeunit' */]);
 };
