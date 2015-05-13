0dd48ffb0e816b7b65c475b1d91629700bbaf73b
Daniel Saxil-Nielsen <daniel@saxil-nielsen.com>
2015-05-13 13:41:28 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,6 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
 			all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
-		},
-		nodeunit: {
-			all: [/*'tests/*.js'*/]
 		}
 	});
 
