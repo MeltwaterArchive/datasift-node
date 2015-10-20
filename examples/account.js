273b4d00d600ff684b8586083810da16ac968be7
Richard Caudle <richard.caudle@datasift.com>
2015-10-20 14:46:43 +0100

    Added support for the account/usage endpoint

---
+++ examples/account.js
@@ -0,0 +1,49 @@
+// #Example - Monitoring account usage
+// *How to get usage details from the API.*
+
+// Require the DataSift library - **choose one of these**:
+var DataSift = require('datasift-node'); // When running from NPM package
+var DataSift = require('../lib/datasift'); // When running within datasift-node repository
+
+// Create a DataSift client object - **insert your API credentials**:
+var ds = new DataSift('YOUR_USERNAME', 'YOUR_APIKEY');
+
+// ## Declare Utility Methods
+// Gets latest usage details from the API
+function latestUsage() {
+	ds.account.usage(function(err, response) {
+		if(err)
+			console.log(err);
+		else
+		{
+			console.log("Latest usage: \n" + JSON.stringify(response));
+			lastMonth();
+		}
+	});
+}
+
+// Gets last month's usage details from the API
+function lastMonth() {
+
+	// First day of last month
+	var d = new Date();
+	d.setMonth(d.getMonth() - 1);
+	d = new Date(d.getFullYear(), d.getMonth(), 1);
+	console.log(d);
+
+	ds.account.usage({ "period": "monthly", "start": (d.getTime() / 1000) }, function(err, response) {
+		if(err)
+			console.log(err);
+		else
+		{
+			console.log("Last month: \n" + JSON.stringify(response));
+		}
+	});
+}
+
+
+// ## Initiate Process
+// Finally we start the process - this will lead to:
+// * Get the latest usage details from the API
+// * Get last month's usage details from the API
+latestUsage();
